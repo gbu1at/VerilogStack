bbadfb205c36ee04c6e2b32aab5aa1de2e1a80c8
apple <bulatchess@mail.ru>
2024-11-13 19:00:52 +0300

    commit

--- stack_behaviour.sv
+++ stack_behaviour.sv
@@ -1,6 +1,5 @@
-
 module stack_behaviour_normal (
-    input wire [3:0] IO_DATA,
+    inout wire [3:0] IO_DATA,
     input wire RESET, 
     input wire CLK, 
     input wire [1:0] COMMAND, 
@@ -9,9 +8,7 @@ module stack_behaviour_normal (
     reg [3:0] curr_idx;
     reg [3:0] stack [4:0];
     reg [3:0] o_data;
-
-    assign IO_DATA = (COMMAND[1] == 0 || CLK == 1'b0) ? 'z : o_data;
-
+    assign IO_DATA = ((COMMAND[1] == 0 || CLK == 1'b0) ? 'z : o_data);
     initial begin
         stack[0] = 0;
         stack[1] = 0;
@@ -21,9 +18,13 @@ module stack_behaviour_normal (
         curr_idx = 4'b0000;
         o_data = 4'b0000;
     end
+
     reg [2:0] i;
-    reg [3:0] shift_indx;
-    always @(posedge CLK, RESET) begin
+
+    reg is_push = 0;
+    reg is_pop = 0;
+
+    always @(RESET, CLK, INDEX, COMMAND, IO_DATA) begin
         if (RESET == 1) begin
             stack[0] = 0;
             stack[1] = 0;
@@ -32,34 +33,40 @@ module stack_behaviour_normal (
             stack[4] = 0;
             curr_idx = 4'b0000; 
             o_data = 4'b0000;
+            is_push = 0;
+            is_pop = 0;
         end
-
-        else begin
+        else if (CLK == 0) begin
+            is_push = 0;
+            is_pop = 0;
+        end
+        else if (CLK == 1) begin
             if (COMMAND == 2'b00) begin 
             end
             if (COMMAND == 2'b01) begin
-                curr_idx += 1;
-                if (curr_idx == 4'b0101)
-                    curr_idx = 0;
-                stack[curr_idx] = IO_DATA;
+                if (is_push == 0) begin
+                    curr_idx += 1;
+                    curr_idx %= 5;
+                    stack[curr_idx] = IO_DATA;
+                    is_push = 1;
+                end
+                else begin 
+                    stack[curr_idx] = IO_DATA;
+                end
             end
             if (COMMAND == 2'b10) begin
-                o_data = stack[curr_idx];
-                if (curr_idx == 0) curr_idx = 4'b0101;
-                curr_idx -= 1;
+                if (is_pop == 0) begin
+                    o_data = stack[curr_idx];
+                    curr_idx += 4;
+                    curr_idx %= 5;
+                    is_pop = 1;
+                end
             end
             if (COMMAND == 2'b11) begin
                 i = INDEX;
-                if (i == 3'b111) i = 3'b010;
-                if (i == 3'b110) i = 3'b001;
+                i %= 5;
                 i = 5 - i;
-                shift_indx = curr_idx + i;
-                if (shift_indx == 4'b1001) shift_indx = 4'b0100;
-                if (shift_indx == 4'b1000) shift_indx = 4'b0011;
-                if (shift_indx == 4'b0111) shift_indx = 4'b0010;
-                if (shift_indx == 4'b0110) shift_indx = 4'b0001;
-                if (shift_indx == 4'b0101) shift_indx = 4'b0000;
-                o_data = stack[shift_indx];
+                o_data = stack[(curr_idx + i) % 5];
             end
         end
     end
